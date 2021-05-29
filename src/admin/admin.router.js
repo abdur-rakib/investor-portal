38277ad66bdb90d0c0c3005fe7ad9ea9520690a6
mainu007 <mainudev.co@gmail.com>
2021-05-29 18:43:01 +0600

    :sparkles: Adding login page

--- src/admin/admin.router.js
+++ src/admin/admin.router.js
@@ -1,7 +1,23 @@
-const { buildRouter } = require('@admin-bro/express');
+const bcrypt = require('bcrypt');
+const { buildAuthenticatedRouter } = require('@admin-bro/express');
+const { User } = require('../models/user');
+
+require('dotenv').config();
 
 const buildAdminRouter = (admin) => {
-    const router = buildRouter(admin);
+    const router = buildAuthenticatedRouter(admin, {
+        authenticate: async (email, password) => {
+            const user = await User.findOne({ email });
+            if (user) {
+                const matched = await bcrypt.compare(password, user.encryptedPassword);
+                if (matched) {
+                    return user;
+                }
+            }
+            return false;
+        },
+        cookiePassword: process.env.COOKIE_PASSWORD,
+    });
     return router;
 };
 
